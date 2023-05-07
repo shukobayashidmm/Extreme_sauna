13be4be3809e2ccf56f45022f59c15d05e9844ef
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-05-07 05:21:15 +0000

    ddevise導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "ac994ebad14f48e79dbc1b624b78db1a.vfs.cloud9.us-east-1.amazonaws.com"
 end
