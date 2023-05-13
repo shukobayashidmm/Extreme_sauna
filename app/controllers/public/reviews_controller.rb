7bde6f0c56c488640b47319d9cc2238be7eabe39
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-05-13 18:02:22 +0000

    ログイン画面

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -16,8 +16,11 @@ class Public::ReviewsController < ApplicationController
         @sauna = Sauna.find(params[:sauna_id])
         review = current_user.reviews.new(review_params)
         review.sauna_id = @sauna.id
-        review.save
-        redirect_to public_sauna_reviews_path
+        if review.save
+           redirect_to public_sauna_reviews_path
+        else
+           redirect_to request.referer
+        end
     end
     
     
@@ -32,10 +35,14 @@ class Public::ReviewsController < ApplicationController
     end
     
     def update
+        @user = current_user.id
         @sauna = Sauna.find(params[:sauna_id])
         review = Review.find(params[:id])
-        review.update(review_params)
-        redirect_to public_sauna_reviews_path
+        if  review.update(review_params)
+            redirect_to public_sauna_reviews_path
+        else
+            redirect_to request.referer
+        end
     end
     
     def destroy
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,7 @@ class Review < ApplicationRecord
     belongs_to :sauna
     belongs_to :user
     has_many :comments, dependent: :destroy
+    
+    validates :content, presence: true
+    validates :star, presence: true
 end
