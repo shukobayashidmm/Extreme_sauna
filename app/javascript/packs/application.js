ee7d875f2676199bf758671d73bc3fa789b75287
shukobayashidmm <shu.kobayashi-0627@yahoo.ne.jp>
2023-05-13 10:31:23 +0000

    評価平均

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
 
 
 
@@ -19,7 +20,7 @@ window.$ = jQuery;
 
 import Raty from "raty.js"
 window.raty = function(elem,opt){
-    let raty =  new Raty(elem,opt)
-    raty.init();
-    return raty;
-}
+     let raty =  new Raty(elem,opt)
+     raty.init();
+     return raty;
+ }
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,4 @@ class Review < ApplicationRecord
     belongs_to :sauna
     belongs_to :user
     has_many :comments, dependent: :destroy
-    rating
 end
